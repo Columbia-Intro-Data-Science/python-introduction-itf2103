64f2cf0b906e0fad5df91776cc295fff5806a67e
itf2103 <itf2103@columbia.edu>
2018-05-09 11:30:56 +0000

    updated final project

--- recipe-recommender-final-project/recipe_recommender.py
+++ recipe-recommender-final-project/recipe_recommender.py
@@ -1,34 +1,43 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
 import pandas
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-#from sqlalchemy import create_engine
-#import psycopg2
 
-#try:
-#    conn = psycopg2.connect("dbname='recipe_recommender' user='postgres' host='localhost' password='recipe' port='5432'")
-#except:
-#    print('Unable to connect to database')   
+@app.route('/')
+def recommend():
+    #from sqlalchemy import create_engine
+    #import psycopg2
 
-#engine = create_engine('postgresql://postgres:recipe@localhost:5432/recipe_recommender')
-#ingredients_matrix = pandas.read_sql_table('Ingredients Matrix', engine)
+    #try:
+    #    conn = psycopg2.connect("dbname='recipe_recommender' user='postgres' host='localhost' password='recipe' port='5432'")
+    #except:
+    #    print('Unable to connect to database')
 
-ingredients_matrix = pandas.read_csv('ingredients_matrix.csv')
+    #engine = create_engine('postgresql://postgres:recipe@localhost:5432/recipe_recommender')
+    #ingredients_matrix = pandas.read_sql_table('Ingredients Matrix', engine)
 
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), stop_words='english', binary=True)
-tfidf_matrix = tf.fit_transform(ingredients_matrix['Ingredients'])
-tfidf_matrix
+    ingredients_matrix = pandas.read_csv('ingredients_matrix.csv')
 
-#cosine similarity
-recipe_comparitor = 1
-cosine_similarities = linear_kernel(tfidf_matrix[recipe_comparitor], tfidf_matrix).flatten()
-cosine_similarities
-print('Comparing recipes to: ' + str(ingredients_matrix['Title'][recipe_comparitor]))
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), stop_words='english', binary=True)
+    tfidf_matrix = tf.fit_transform(ingredients_matrix['Ingredients'])
+    tfidf_matrix
 
-cosine_index = cosine_similarities.argsort()[:-12:-1] # Return the 10 best matches for recipes not including recipe used for comparison
+    #cosine similarity
+    recipe_comparitor = 1
+    cosine_similarities = linear_kernel(tfidf_matrix[recipe_comparitor], tfidf_matrix).flatten()
+    cosine_similarities
+    print('Comparing recipes to: ' + str(ingredients_matrix['Title'][recipe_comparitor]))
 
-similar_items = []
-for i in range(len(cosine_index)):
-    similar_items.append([(ingredients_matrix['Title'][cosine_index[i]]), cosine_similarities[cosine_index[i]]])
-del similar_items[0] # Delete first item from list as that will be the recipe being used for comparison
-print('Showing 10 best recipe matches and the cosine similarity')
-print(similar_items)
+    cosine_index = cosine_similarities.argsort()[:-12:-1] # Return the 10 best matches for recipes not including recipe used for comparison
+
+    similar_items = []
+    for i in range(len(cosine_index)):
+        similar_items.append([(ingredients_matrix['Title'][cosine_index[i]]), cosine_similarities[cosine_index[i]]])
+    del similar_items[0] # Delete first item from list as that will be the recipe being used for comparison
+    return jsonify('Showing 10 best recipe matches and the cosine similarity', similar_items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
